ce2fa5ad397c9605bb3b5b67fc40ce10bac8408c
Happy-Ferret <MarkBauer@gmx.ch>
2019-08-10 18:52:49 +0200

    Code Generator: Revise direction scheme
    Change in-between directions to closest cardinal directions,
    mirroring Trizbort 1.5.8+.

--- src/codegen/CodeGenerator.ts
+++ src/codegen/CodeGenerator.ts
@@ -71,24 +71,27 @@ export class CodeGenerator {
     return new Handlebars.SafeString(this.capitalize(this.camelCase(this.removeSpecialChars(this.removeAccents(str)))));
   }  
 
+  // v0.2.0
+  // Changed in-between directions to closest cardinal directions.
+  // See: https://www.trizbort.com/Docs/index.shtml#help_conn_ports
   protected dirToStr(dir: Direction): string {
     switch(dir) {
       case Direction.N:   return "north";
-      case Direction.NNE: return "northnortheast";
+      case Direction.NNE: return "north";
       case Direction.NE:  return "northeast";
-      case Direction.ENE: return "eastnortheast";
+      case Direction.ENE: return "east";
       case Direction.E:   return "east";
-      case Direction.ESE: return "eastsoutheast";
+      case Direction.ESE: return "east";
       case Direction.SE:  return "southeast";
-      case Direction.SSE: return "southsoutheast";
+      case Direction.SSE: return "south";
       case Direction.S:   return "south";
-      case Direction.SSW: return "southsouthwest";
+      case Direction.SSW: return "south";
       case Direction.SW:  return "southwest";
-      case Direction.WSW: return "westsouthwest";
+      case Direction.WSW: return "west";
       case Direction.W:   return "west";
-      case Direction.WNW: return "westnorthwest";
+      case Direction.WNW: return "west";
       case Direction.NW:  return "northwest";
-      case Direction.NNW: return "northnorthwest";         
+      case Direction.NNW: return "north";
       default: return "";
-    }    
+    }
   }}
