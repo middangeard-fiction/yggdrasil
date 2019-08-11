8dd637dae7b9c50b1427433b808ddbb0dab25faf
Happy-Ferret <MarkBauer@gmx.ch>
2019-08-11 19:37:02 +0200

    Update Astilectron typings
    Add "showOpenDialog" type definition.
    
    Update "sendMessage" type definition.
    => Callback function should be optional.

--- src/typings/astilectron.d.ts
+++ src/typings/astilectron.d.ts
@@ -1,4 +1,5 @@
 declare class astilectron {
+    static showOpenDialog(options: object, arg1: (paths: Array<string>) => void): string;
     static onMessage(fn: Function): any;
-    static sendMessage(name: string, fn: Function): any;
+    static sendMessage(name: string | object, fn?: Function): any;
 }
