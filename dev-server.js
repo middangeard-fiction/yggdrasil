0d1ea7bbd63615ea6bd3ab7e6c8592b1c17cc2b0
Happy-Ferret <MarkBauer@gmx.ch>
2019-08-22 10:24:26 +0200

    Dev-server: Enable static files
    In preparation for i18n "locales".

--- dev-server.js
+++ dev-server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("./"));
+
 app.get('/dist/:name', function (req, res) {
     res.sendFile(req.params.name, { root: __dirname + "/dist/" });
 });
