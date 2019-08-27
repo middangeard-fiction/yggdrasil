d67594202bace466db65d483c50f91ebd5137085
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2019-08-27 11:08:27 +0200

    Add ZIL code generation

--- src/panels/menuPanel/menuPanel.ts
+++ src/panels/menuPanel/menuPanel.ts
@@ -9,7 +9,7 @@ import { Map } from '../../models/map';
 import { Exporter } from '../../exporter';
 
 import { CodeGenerator, TadsGenerator, Inform7Generator, Alan2Generator, Alan3Generator, 
-         QuestGenerator, Middangeard1Generator, TextadventurejsGenerator } from '../../codegen/CodeGeneration'
+         QuestGenerator, Middangeard1Generator, TextadventurejsGenerator, ZilGenerator } from '../../codegen/CodeGeneration'
 
 import { IdToast } from '../../controls/controls';
 
@@ -43,7 +43,8 @@ export class MenuPanel extends Panel {
     this.createMenuItem('#menu-export-alan2', () => { this.actionGenerateCode(new Alan2Generator(App.map), 'a2c'); });
     this.createMenuItem('#menu-export-alan3', () => { this.actionGenerateCode(new Alan3Generator(App.map), 'a3c'); });
     this.createMenuItem('#menu-export-quest', () => { this.actionGenerateCode(new QuestGenerator(App.map), 'aslx'); });
-    this.createMenuItem('#menu-export-textadventurejs', () => { this.actionGenerateCode(new TextadventurejsGenerator(App.map), 'txt'); });
+    this.createMenuItem('#menu-export-textadventurejs', () => { this.actionGenerateCode(new TextadventurejsGenerator(App.map), 'js'); });
+    this.createMenuItem('#menu-export-zil', () => { this.actionGenerateCode(new ZilGenerator(App.map), 'zil'); });
 
     this.inputLoad.addEventListener('change', this.handleInputLoad);
     this.inputImport.addEventListener('change', this.handleInputImport);
